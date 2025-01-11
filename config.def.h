774a7ffeb4ff30f64b0c8add6fe4106d8ed90aa5
Chucklee1 - remote <cooperkang4@gmail.com>
2025-01-11 15:15:01 -0800

    more typo fixes

--- config.def.h
+++ config.def.h
@@ -73,11 +73,11 @@ static Key keys[] = {
   { 0,               XF86XK_MonBrightnessUp, spawn,          {.v = inclight } },
   { 0,             XF86XK_MonBrightnessDown, spawn,          {.v = declight } },
   { MODKEY,                       XK_Up,     focusstack,     {.i = +1 } }, /* windows */
-  { MODKEY,                       XK_Down    focusstack,     {.i = -1 } },
+  { MODKEY,                       XK_Uown,   focusstack,     {.i = -1 } },
   { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
   { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-  { MODKEY,                       XK_Equals, setmfact,       {.f = -0.05} },
-  { MODKEY,                       XK_Minus,  setmfact,       {.f = +0.05} },
+  { MODKEY,                       XK_equal,  setmfact,       {.f = -0.05} },
+  { MODKEY,                       XK_minus,  setmfact,       {.f = +0.05} },
   { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
   { MODKEY,                       XK_Left    shiftview,      { .i = +1 } }, /* tags */
   { MODKEY|ShiftMask,             XK_Left,   shiftswaptags,  { .i = +1 } },
