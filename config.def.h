e710b29d748008cc79c9b46bcbf0f1d9d8a8a828
Chucklee1 - remote <cooperkang4@gmail.com>
2025-01-12 03:21:53 -0800

    updated colors

--- config.def.h
+++ config.def.h
@@ -5,12 +5,12 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMonoNerdFontMono:size=10" };
-static const char dmenufont[]       = "JetBrainsMonoNerdFontMono:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=10"  };
+static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=10" ;
+static const char col_gray1[]       = "#202020";
+static const char col_gray2[]       = "#303030";
+static const char col_gray3[]       = "#B0B0B0";
+static const char col_gray4[]       = "#E0E0E0";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
