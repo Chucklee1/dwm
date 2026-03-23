c7a55f719e5b002c844e5983e4f1f0de88466843
Chucklee1 <kermitthefrog@kakao.com>
2026-03-23 08:35:22 -0700

    new: simple c++ code to generate base16 palette from stylix
    new file:   lib/genPalette.cpp
    new file:   lib/justfile
    new file:   palette.h

---
+++ palette.h
@@ -0,0 +1,19 @@
+#ifndef PALETTE_H
+#define PALETTE_H
+	static const char base0F[] = "#5e81ac";
+	static const char base0E[] = "#b48ead";
+	static const char base0D[] = "#81a1c1";
+	static const char base00[] = "#2e3440";
+	static const char base01[] = "#3b4252";
+	static const char base02[] = "#434c5e";
+	static const char base03[] = "#4c566a";
+	static const char base04[] = "#d8dee9";
+	static const char base0B[] = "#a3be8c";
+	static const char base05[] = "#e5e9f0";
+	static const char base06[] = "#eceff4";
+	static const char base08[] = "#bf616a";
+	static const char base07[] = "#8fbcbb";
+	static const char base09[] = "#d08770";
+	static const char base0C[] = "#88c0d0";
+	static const char base0A[] = "#ebcb8b";
+#endif
